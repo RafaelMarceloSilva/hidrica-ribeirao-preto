937f536b4be15fc84e4d0c9dec8cdb9dca1653ea
RafaelMarceloSilva <127117996+RafaelMarceloSilva@users.noreply.github.com>
2025-08-22 16:48:12 -0300

    Enviando Arquivos p1

---
+++ mapa.js
@@ -0,0 +1,8 @@
+var map = L.map('map', {
+    dragging: false,
+    zoomControl: false,
+    scrollWheelZoom: false,
+    doubleClickZoom: false,
+    touchZoom: false,
+    keyboard: false,
+}).setView([-21.1784, -47.8066], 20);
